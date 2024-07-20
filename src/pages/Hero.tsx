8bbe163ca2f37c4c990dec5cc6db6d6ca6f98222
moink7 <140623854+moink7@users.noreply.github.com>
2024-07-21 00:40:31 +0530

    first commit

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react'
 import NavbarDemo from './NavbarDemo'
 import { FaArrowRight } from 'react-icons/fa6'
--- src/pages/NavbarDemo.tsx
+++ src/pages/NavbarDemo.tsx
@@ -1,4 +1,4 @@
-
+"use client"
 import {  NavHashLink } from 'react-router-hash-link';
 
 
